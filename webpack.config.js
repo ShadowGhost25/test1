139cd61500d2ef4d96aefccb5a8eef53206a3a46
Ilya Vavilin <ilya.vavilin@corp.codeinside.ru>
2025-05-23 14:33:52 +0300

    firstcommit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // Точка входа для сборки проекта
+
+  output: {
+    filename: 'bundle.js', // Имя выходного файла сборки
+    path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Добавляем .jsx в разрешаемые расширения
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
+        use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ],
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Каталог для статики
+    },
+    compress: true,
+    port: 3000,
+    open: true, // Автоматически открывать браузер
+  },
+
+  mode: 'development', // Режим сборки
+};
